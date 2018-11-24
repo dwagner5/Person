c924a8aa68394bc2b46a1fd2e2744992064461fb
dakota <electribemaster@gmail.com>
2018-11-24 17:24:40 -0600

    Add files via upload

--- Person.cs
+++ Person.cs
@@ -1,108 +1,159 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-class Person
+namespace ObjectOrientedProjectC
 {
-	private string _firstName;
-	private string _lastName;
-	private string _SSN;
-	private string _gender;
-	
-	public Person(firstName, lastName, SSN, gender)
-	{
-		_firstName = firstName;
-		_lastName = lastName;
-		_SSN = SSN;
-		_gender = gender;
-	}
-	
-	public void pay()
-	{
-		println(_firstName + _lastName + " got paid.");
-	}
-	
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _SSN;
 
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string SSN
+        {
+            get { return _SSN; }
+            set { _SSN = value; }
+        }
+
+    }
+    class Establishment : Person
+    {
+        private string _businessName;
+
+        public string BusinessName
+        {
+            get { return _businessName; }
+            set { _businessName = value; }
+        }
+
+    }
+
+    class Employee : Establishment
+    {
+        private bool _isSalary;
+        private string _department;
+        private string _title;
+
+        public bool IsSalary
+        {
+            get { return _isSalary; }
+            set { _isSalary = value; }
+        }
+        public string Department
+        {
+            get { return _department; }
+            set { _department = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public void Pay(double payRate)
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + "\nEstablishment: " + BusinessName);
+            Console.WriteLine("Salary: " + IsSalary + "\nDepartment: " + Department);
+            Console.WriteLine("Title: " + Title);
+            if (IsSalary == true)
+            {
+                Console.WriteLine("Paid " + "$" + (payRate / 12.0).ToString("F2")); // salary divided into months
+            }
+            else
+            {
+                Console.WriteLine("Paid " + "$" + (payRate * 160.0).ToString("F2")); // 40 hr workweek x 4
+            }
+        }
+    }
+
+    class Management : Employee
+    {
+        public Management()
+        {
+            Title = "Management";
+            IsSalary = true;
+        }
+    }
+    class Staff : Employee
+    {
+        public Staff()
+        {
+            Title = "Staff";
+            IsSalary = true;
+        }
+
+    }
+    class Temp : Employee
+    {
+        public Temp()
+        {
+            Title = "Temp";
+            IsSalary = false;
+        }
+    }
+    class Customer : Establishment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("I am a customer, and cannot be paid.");
+        }
+
+    }
+    class Student : Establishment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("I am a student, and cannot be paid.");
+        }
+
+    }
+    class Administration : Employee
+    {
+        public Administration()
+        {
+            Title = "Administration";
+            IsSalary = true;
+        }
+
+    }
+    class Faculty : Employee
+    {
+        public Faculty()
+        {
+            Title = "Faculty";
+            IsSalary = true;
+        }
+
+    }
+    class FullTime : Faculty
+    {
+        public FullTime()
+        {
+            Title = "FullTime";
+            IsSalary = true;
+        }
+
+    }
+    class Adjunct : Faculty
+    {
+        public Adjunct()
+        {
+            Title = "Adjunct";
+            IsSalary = false;
+        }
+
+    }
 }
-// Business Classes
-class Business : Person
-{
-	private string _businessName = "Dakota Central Telecommunications";
-
-}
-class Employee : Business
-{
-	private int _employeeID;
-	private bool _isSalary;
-	private string _department;
-	private string _title;
-
-}
-class Management : Employee
-{
-	private int _salary = 40000;
-
-}
-class Staff : Employee
-{
-	private int _salary = 40000;
-
-}
-class Temp : Employee
-{
-	private int _hourlyRate = 20;
-
-}
-class Customer : Business
-{
-	private int _customerID;
-
-}
-// School Classes
-class School : Person
-{
-	private string _schoolName = "University of Jamestown";
-}
-class Student : School
-{
-	private int _studentID;
-
-}
-class SchoolEmployee : School
-{
-	private int _employeeID;
-	private string _department;
-	private bool _isSalary;
-	private string _title;
-	
-
-}
-class Administration : SchoolEmployee
-{
-	private int _salary = 40000;
-
-}
-class Faculty : SchoolEmployee
-{
-	private bool _isFullTime;
-
-}
-class FullTime : Faculty
-{
-	private int _salary = 40000;
-
-}
-class Adjunct : Faculty
-{
-	private int _hourlyWage = 20;
-
-}
-class Staff : SchoolEmployee
-{
-	private int _salary = 40000;
-
-}
-
-
-
-
-
-
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ObjectOrientedProjectC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is Dakota Wagner's Class Inheritance Project.\n");
+
+            // Management example - utilizes the Salary aspect of the Pay() method
+            Management Dakota = new Management();
+            Dakota.FirstName = "Dakota";
+            Dakota.LastName = "Wagner";
+            Dakota.BusinessName = "JPS";
+            Dakota.Department = "Computer Science";
+            Dakota.Pay(45000); // 45,000 yearly divided by 12
+
+            Console.WriteLine();
+
+            // Staff example - utilizes the Salary aspect of the Pay() method
+            Staff Bob = new Staff();
+            Bob.FirstName = "Bob";
+            Bob.LastName = "McGee";
+            Bob.BusinessName = "JPS";
+            Bob.Department = "Maintenance";
+            Bob.Pay(30000); // 30,000 yearly divided by 12
+
+            Console.WriteLine();
+
+            // Temp example - utilizes the non Salary aspect of the Pay() method
+            Temp Jack = new Temp();
+            Jack.FirstName = "Jack";
+            Jack.LastName = "Johnson";
+            Jack.BusinessName = "JPS";
+            Jack.Department = "Camel Artisan";
+            Jack.Pay(15.00); // 15.00 an hour
+
+            Console.WriteLine();
+
+            // Administration example - utilizes the Salary aspect of the Pay() method
+            Administration Billy = new Administration();
+            Billy.FirstName = "Billy";
+            Billy.LastName = "Bob";
+            Billy.BusinessName = "UJ";
+            Billy.Department = "IT";
+            Billy.Pay(50000); // 50,000 yearly divided by 12
+
+            Console.WriteLine();
+
+            // Staff example - utilizes the Salary aspect of the Pay() method
+            Staff Jackie = new Staff();
+            Jackie.FirstName = "Jackie";
+            Jackie.LastName = "McGee";
+            Jackie.BusinessName = "UJ";
+            Jackie.Department = "IT";
+            Jackie.Pay(40000); // 40,000 yearly divided by 12
+
+            Console.WriteLine();
+
+            // FullTime Faculty example - utilizes the Salary aspect of the Pay() method
+            FullTime Jim = new FullTime();
+            Jim.FirstName = "Jim";
+            Jim.LastName = "Woom";
+            Jim.BusinessName = "UJ";
+            Jim.Department = "Physical Education";
+            Jim.Pay(30000); // 30,000 yearly divided by 12
+
+            Console.WriteLine();
+
+            // PartTime Faculty example - utilizes the Salary aspect of the Pay() method
+            Adjunct Sharon = new Adjunct();
+            Sharon.FirstName = "Sharon";
+            Sharon.LastName = "McAppleJacks";
+            Sharon.BusinessName = "UJ";
+            Sharon.Department = "Arts";
+            Sharon.Pay(20.00); // 20.00 hourly 
+
+            Console.WriteLine();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
